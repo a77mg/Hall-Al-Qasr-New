0562fac57835376660c0714e03fbd4e6dabb9e86
a77mg <157146178+a77mg@users.noreply.github.com>
2026-07-13 04:37:30 +0300

    Configure dependency resolution management
    Added dependency resolution management with repositories and version catalogs.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
+}
+
 pluginManagement {
   repositories {
     google {
